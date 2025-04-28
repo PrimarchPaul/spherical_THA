db5d3e9088c0e50d7958eb6db6c3853cb9fe0ad8
PrimarchPaul <preye032@fiu.edu>
2025-04-28 03:39:40 -0400

    prod database hotfix

--- Backend/src/services/database.ts
+++ Backend/src/services/database.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 //for local development
 
+/*
 const sequelize = new Sequelize(
     process.env.DB_POSTGRESQL_DATABASE as string,
     process.env.DB_POSTGRESQL_USERNAME as string,
@@ -35,10 +36,10 @@ const testConnection = async () => {
 export { sequelize, testConnection };
 
 
-
+*/
 //for production
 
-/*
+
 const conn = process.env.DB_CONNECTION as string;
 if(!conn) {
     throw new Error("DB_CONNECTION is not set");
@@ -64,4 +65,3 @@ const testConnection = async () => {
 }
 
 export { sequelize, testConnection };
-*/
